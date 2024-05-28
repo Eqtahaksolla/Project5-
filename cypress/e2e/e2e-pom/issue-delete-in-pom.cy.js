6de6984b36a10dadd59d876c3cc1951a5658865d
Eqtahaksolla <raabisegert@gmail.com>
2024-05-28 20:11:52 +0300

    new

--- cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
@@ -5,6 +5,7 @@ import IssueModal from "../../pages/IssueModal";
 
 describe('Issue delete', () => {
   beforeEach(() => {
+    Cypress.config("defaultCommandTimeout", 60000);
     cy.visit('/');
     cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
     //open issue detail modal with title from line 16  
@@ -16,10 +17,15 @@ describe('Issue delete', () => {
   const issueTitle = 'This is an issue of type: Task.';
 
   it('Should delete issue successfully', () => {
-    //add steps to delete issue
+    IssueModal.clickDeleteButton
+    IssueModal.confirmDeletion
+    IssueModal.ensureIssueIsNotVisibleOnBoard
+    
   });
 
   it('Should cancel deletion process successfully', () => {
-    //add steps to start deletion proces but cancel it
+    IssueModal.clickDeleteButton
+    IssueModal.cancelDeletion
+    IssueModal.ensureIssueIsVisibleOnBoard
   });
 });
